dca0f2daddb15017a55f247cdc77e39d094d5530
alyazeeditalal <talal.ali92@gmail.com>
2023-10-30 12:58:20 +0000

    update pipeline

---
+++ pipeline/step_00_merge_fasta_fies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#In this script I will merge the fasta fies of each pair if there are more than one file per pair 
+########################################################################
+
+#output directory
+output_dir='../1_data' #Directory were the fasta files will be saved 
+#input directory
+input_dir='../sequences' #Directory contaiing subdirectories with fasta files 
+
+#merge fasta files that are of the same pair 
+for dir in $input_dir/*; do 
+individual=$(basename $(echo $dir))
+
+ for fa in $individual; do 
+  #cat $input_dir/${individual}/*_1.fq.gz >  $output_dir/${individual}_1.fq.gz
+  cat $input_dir/${individual}/*_2.fq.gz >  $output_dir/${individual}_2.fq.gz
+ done
+done 
